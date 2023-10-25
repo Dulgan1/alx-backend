fe30883c7b10b927abe7f9fa5e6bfa01d25a2e2f
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-25 22:13:14 +0100

    Initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Class BasicCache: a basic cache system"""
+
+BaseCache = __import__('base_cache').BaseCache
+
+
+class BasicCache(BaseCache):
+    """
+    Basic Caching system: uses self.cache_data of parent
+    methods:
+        get : gets cached data
+        put : sets cache data
+    """
+
+    def put(self, key, item):
+        """Sets data with key and item into cache data """
+        if key and item:
+            self.cache_data.update({key : item})
+
+    def get(self, key):
+        """Gets data with key in cache data"""
+        if not key:
+            return None
+        return self.cache_data.get(key) if key in self.cache_data else None
