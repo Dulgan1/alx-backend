c7d205398dcaa81822105d5adf6ff435f38c0309
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-25 23:21:12 +0100

    Initial commit

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""FIFOCache: FIFO Caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Implements the FIFO Cache system"""
+    def __init__(self):
+        super().__init__()
+        self.keys = [key for key in self.cache_data]
+
+    def put(self, key, item):
+        """Sets new data into cache data"""
+        if key and item:
+            if len(self.cache_data) == self.MAX_ITEMS and \
+                    key not in self.keys:
+                ex_key = self.keys.pop(0)
+                del self.cache_data[ex_key]
+                print("DISCARD: {}".format(ex_key))
+            self.keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets data in cache with key"""
+        return self.cache_data.get(key) if key and \
+                key in self.cache_data else None
