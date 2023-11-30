c750099e5390d7286ca30771525bd4af52550980
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-29 16:36:36 -0800

    Initial commit

---
+++ 0x03-queuing_system_in_js/redis-6.0.10/0-redis_client.js
@@ -0,0 +1,5 @@
+import { createClient } from 'redis';
+
+const rClient = await createClient();
+rClient.on('connect', () => {console.log('Redis client connected to the server')});
+rClient.on('error', (error) => {console.log(`Redis client not connected to the server: ${error.message}`)});
