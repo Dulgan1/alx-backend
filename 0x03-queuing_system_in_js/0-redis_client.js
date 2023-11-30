c331bd5e79418865df1e7068be95618bc9393a1c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-29 17:03:01 -0800

    No need for await

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,5 +1,5 @@
 import { createClient } from 'redis';
 
-const rClient = await createClient();
+const rClient = createClient();
 rClient.on('connect', () => {console.log('Redis client connected to the server')});
 rClient.on('error', (error) => {console.log(`Redis client not connected to the server: ${error.message}`)});
